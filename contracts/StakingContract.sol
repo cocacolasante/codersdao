91f6169b0e9f5399ca5637d16d3dffc89c190318
cocacolasante <105400476+cocacolasante@users.noreply.github.com>
2022-10-28 21:13:02 -0400

    staking function untested

--- contracts/StakingContract.sol
+++ contracts/StakingContract.sol
@@ -10,9 +10,29 @@ import "./CodersCrypto.sol";
 import "./CodersNFT.sol";
 
 contract StakingContract{
-    ERC721[] public stakingNFTs;
+    ERC721 public stakingNFT;
     ERC20 public rewardsToken;
     address payable public admin;
+
+    uint public rewardRate = 1;
+
+    // uint256 public stakeDuration = 2592000; // 30 days (30 * 24 * 60 * 60)
+
+
+    // mapping of rewards to tokens
+    mapping(address=>uint) public usersRewards;
+
+    // mapping of address to stake info
+    mapping(address => StakeInfo) public usersStakes;
+
+    struct StakeInfo{
+        uint256 startTime;
+        uint256 endTime;        
+        uint256 tokenId; 
+        uint256 amountClaimed; 
+    }
+
+    event Staked(address indexed staker, uint indexed tokenId);
 
 
     modifier onlyAdmin {
@@ -25,16 +45,40 @@ contract StakingContract{
         admin = payable(msg.sender);
     }
 
+    // UPDATER FUNCTIONS
     // add a new nft contract for staking
     function addNftContract(ERC721 newNFT) public onlyAdmin {
-        stakingNFTs.push(newNFT);
+        stakingNFT = newNFT;
     }
 
     function setRewardsToken(ERC20 newRewardsToken) public onlyAdmin {
         rewardsToken = newRewardsToken;
     }
 
+    function updateRewardRate(uint newRate) public onlyAdmin {
+        rewardRate = newRate;
+    }
 
+     // staking function - duration needs to convert from days to seconds
+    function stakeNFT(uint tokenId, uint duration) external {
+        require(stakingNFT.ownerOf(tokenId) == msg.sender, "not owner of token");
+        // conversion from days to seconds
+        uint convertedDuration = duration * 24 * 60 * 60;
+
+        usersStakes[msg.sender] = StakeInfo(
+            block.timestamp,
+            (block.timestamp + convertedDuration),
+            tokenId,
+            0
+            );
+        
+
+        stakingNFT.transferFrom(msg.sender, address(this), tokenId);
+
+        emit Staked(msg.sender, tokenId);
+    }
+
+    
 
 
 }
